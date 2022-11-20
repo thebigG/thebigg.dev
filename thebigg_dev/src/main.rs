9e70c6acf6bfea06f47959fc91c42230c27ed1c4
thebigG <yunior.eury@gmail.com>
2022-11-20 01:30:26 -0600

    -Nav bar works now.

--- thebigg_dev/src/main.rs
+++ thebigg_dev/src/main.rs
@@ -79,7 +79,7 @@ impl Model {
         html! {
             <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
                 <div class="navbar-brand">
-                    <h1 class="navbar-item is-size-3">{ "Yew Blog" }</h1>
+                    <h1 class="navbar-item is-size-3">{ "thebigg.dev" }</h1>
 
                     <button class={classes!("navbar-burger", "burger", active_class)}
                         aria-label="menu" aria-expanded="false"
